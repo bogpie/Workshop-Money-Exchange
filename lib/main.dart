53a8c25847b329e9e7808eeb58de423d1c13ac2b
Diana Simion <dianamariasimion@gmail.com>
2021-12-20 13:56:34 +0200

    Add setState, controller and error check

--- lib/main.dart
+++ lib/main.dart
@@ -9,21 +9,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.green,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -33,16 +23,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -52,6 +32,48 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   // 1. Create a variable which will change
   String _changeable = 'Initial text';
+  String imageUrl =
+      'https://www.pngall.com/wp-content/uploads/2018/05/Euro-Symbol-PNG-Image-File.png';
+  final TextEditingController _controller = TextEditingController();
+  double _sumBeforeConversion = 0.0;
+  double _sumAfterConversion = 0.0;
+  String _currentCurrency = '';
+  double leiToEurosFactor = 4.9;
+  double leiToDollarsFactor = 4.4;
+  Map<String, double> map = {
+    'euros': 4.9,
+    'dollars': 4.4,
+  };
+  String? _errorText;
+
+  // map  K  V
+  // 'mar' : 'aliment ...'
+  // 'a manca' : 'actiune'
+  //
+  // 'euros' : eurosfactor
+  // 'dollar' : dollarsfactor
+  //
+  // map['mar'] -> 'aliment..'
+  void _changeMoney(String currency) {
+    setState(() {
+      _sumBeforeConversion = double.tryParse(_controller.value.text) ?? 0;
+      _currentCurrency = currency;
+      _sumAfterConversion = _sumBeforeConversion / map[currency]!;
+
+      if (_controller.value.text.isEmpty) {
+        _errorText = 'Please insert a sum';
+      } else {
+        _errorText = null;
+      }
+
+      // before map:
+      // if (currency == 'euros') {
+      //   _sumAfterConversion = _sumBeforeConversion / leiToEurosFactor;
+      // } else if (currency == 'dollars') {
+      //   _sumAfterConversion = _sumBeforeConversion / leiToDollarsFactor;
+      // }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -65,9 +87,16 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              Image.network(
+                imageUrl,
+                height: 200,
+              ),
               TextField(
-                // 3. Change that variable...
-                decoration: const InputDecoration(hintText: 'Enter a text'),
+                // 3. Change that variable with SETSTATE
+                decoration: InputDecoration(
+                  hintText: 'Enter a text',
+                  errorText: _errorText,
+                ),
 
                 keyboardType: const TextInputType.numberWithOptions(
                   signed: false,
@@ -78,13 +107,16 @@ class _MyHomePageState extends State<MyHomePage> {
                   FilteringTextInputFormatter.allow(RegExp('[0-9,.]+')),
                 ],
 
-                onChanged: (enteredText) {
+                onChanged: (text) {
                   setState(
                     () {
-                      _changeable = enteredText;
+                      _changeable = text;
+                      _errorText = null;
                     },
                   );
                 },
+
+                controller: _controller,
               ),
               const SizedBox(
                 height: 8,
@@ -96,23 +128,36 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-
                 children: [
                   ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      _changeMoney('euros');
+                    },
                     child: const Text('Convert to euros'),
                   ),
-
                   const SizedBox(
                     width: 8,
                   ),
-
                   ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () => _changeMoney('dollars'),
                     child: const Text('Convert to dollars'),
                   ),
                 ],
               ),
+              Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Text(
+                        'Before: ${_sumBeforeConversion.toStringAsFixed(2)} lei'),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Text(
+                        'After: ${_sumAfterConversion.toStringAsFixed(2)} $_currentCurrency'),
+                  ),
+                ],
+              )
             ],
           ),
         ),
